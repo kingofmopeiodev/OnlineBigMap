9ddd39e204295aa36d3763d2d2687c5fdde06a76
BuildTools <unconfigured@null.spigotmc.org>
2023-03-04 13:53:33 +0100

    Add project files.

---
+++ Counter.cs
@@ -0,0 +1,49 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBigMap
+{
+    internal class Counter
+    {
+        int counter_index = 0;
+        int counterTimer = Game.GameTime;
+
+        public bool finished;
+        public int secondsToCount;
+
+        public Counter(int secondsToCount)
+        {
+            this.secondsToCount = secondsToCount;
+        }
+
+        public void Count()
+        {
+            switch (counter_index)
+            {
+                case 0:
+                    if (secondsToCount != 0 && counterTimer < Game.GameTime)
+                    {
+                        secondsToCount--;
+                        counterTimer = Game.GameTime + 1000;
+                    }
+                    if (secondsToCount == 0)
+                    {
+                        counter_index = 1;
+                    }
+                    break;
+                case 1:
+                    this.finished = true;
+                    break;
+            }
+        }
+        public void Reset()
+        {
+            counter_index = 0;
+            this.finished = false;
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,57 @@
+﻿using GTA;
+using GTA.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineBigMap
+{
+    public class main : Script
+    {
+        bool bigMapActive = false;
+        Counter MapCounter = new Counter(5);
+        public main()
+        {
+            Tick += onTick;
+            Aborted += onShutDown;
+        }
+        public void onTick(object sender, EventArgs e)
+        {
+            if (bigMapActive)
+            {
+                if (!MapCounter.finished)
+                {
+                    MapCounter.Count();
+                }
+                else
+                {
+                    Function.Call(Hash.SET_BIGMAP_ACTIVE, false, false);
+                    bigMapActive = false;
+                    MapCounter.Reset();
+                    MapCounter = new Counter(5);
+                }
+            }
+            if(Game.IsControlJustPressed(GTA.Control.HUDSpecial))
+            {
+                if (!bigMapActive)
+                {
+                    Function.Call(Hash.SET_BIGMAP_ACTIVE, true, false);
+                    bigMapActive = true;
+                }
+                else
+                {
+                    Function.Call(Hash.SET_BIGMAP_ACTIVE, false, false);
+                    bigMapActive = false;
+                    MapCounter = null;
+                }
+            }
+        }
+        public void onShutDown(object sender, EventArgs e)
+        {
+            Function.Call(Hash.SET_BIGMAP_ACTIVE, false, false);
+        }
+    }
+}
