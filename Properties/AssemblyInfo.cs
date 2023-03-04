ac67e3509909fc59b1c6264262c11545d77e6431
kingofmopeiodev <43313138+kingofmopeiodev@users.noreply.github.com>
2023-03-04 14:18:55 +0100

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OnlineBigMap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small and simple script that adds the big map from GTA Online.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mechagun")]
 [assembly: AssemblyProduct("OnlineBigMap")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
